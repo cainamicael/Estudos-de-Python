6a75ddb5bba33585d53be53c0d69d82e1c421672
cainamicael <cainamicaeloficial@gmail.com>
2022-12-21 11:54:34 -0300

    add

--- Aulas/Aula13/Aula13.py
+++ Aulas/Aula13/Aula13.py
@@ -1,4 +1,7 @@
-num1 = input("Digite o primeiro valor: ")
-num2 = input("Digite o segunto valor.: ")
+import os
+os.system('cls')
+
+num1 = int(input("Digite o primeiro valor: "))# Tem q ter o casting
+num2 = int(input("Digite o segunto valor.: "))
 res = num1 + num2
 print(f'A soma dos valores é: {res}')
