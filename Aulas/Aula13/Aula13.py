d878946253445fb88e20d2882a1aa1100192d128
cainamicael <cainamicaeloficial@gmail.com>
2022-12-21 11:51:57 -0300

    input

--- Aulas/Aula13/Aula13.py
+++ Aulas/Aula13/Aula13.py
@@ -1,1 +1,4 @@
-nome = str(input('Digite o seu nome: '))
+num1 = input("Digite o primeiro valor: ")
+num2 = input("Digite o segunto valor.: ")
+res = num1 + num2
+print(f'A soma dos valores é: {res}')
