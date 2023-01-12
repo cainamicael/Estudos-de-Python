2b8abdda0d256ac18f9487d9bbb816f390021a63
cainamicael <cainamicaeloficial@gmail.com>
2023-01-12 16:14:07 -0300

    add

--- Aulas/Api com Banco de Dados (PycodeBR)/Api_de_Carros.py
+++ Aulas/Api com Banco de Dados (PycodeBR)/Api_de_Carros.py
@@ -4,17 +4,13 @@ from bd_fake import carros #Para usar o banco de dados fake
 
 conexao = sqlite3.connect('C:\\Users\\cmcai\\Documents\\GitHub\\Estudos de Python\\Estudos-de-Python\\Aulas\\Banco\\bd_carros.db', check_same_thread=False) #Esse segundo parâmetro é para não dar o erro de thread
 
-app = Flask(__name__)
-app.config['JSON_SORT_KEYS'] = False #Para não deixar em ordem alfabética
+global carros
+carros = list()
 
-#Consultar carros
-@app.route('/carros', methods=['GET'])
-def get_carros():
+def consultar():
     c = conexao.cursor()
     c.execute("SELECT * FROM carros")
     meus_carros = c.fetchall()
-
-    carros = list()
     for carro in meus_carros:
         carros.append({
             'id' : carro[0],
@@ -22,22 +18,34 @@ def get_carros():
             'modelo' : carro[2],
             'ano' : carro[3]
         })
+    return carros
 
-        return make_response(
-        jsonify(
-            mensagem = 'Lista de Carros',
-            dado = carros
-            )
+def inserir(carro):
+    c = conexao.cursor()
+    c.execute(f"INSERT INTO carros (marca, modelo, ano) VALUES ('{carro.get('marca')}', '{carro.get('modelo')}', '{carro.get('ano')}')")
+    conexao.commit()
+
+
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False #Para não deixar em ordem alfabética
+
+#Consultar carros
+@app.route('/carros', methods=['GET'])
+def get_carros():
+    carros = consultar()
+
+    return make_response(
+    jsonify(
+        mensagem = 'Lista de Carros',
+        dado = carros
         )
+    )
 
 #Criar carro
 @app.route('/carros', methods=['POST'])
 def create_carro():
     carro = request.json
-
-    c = conexao.cursor()
-    c.execute(f"INSERT INTO carros (marca, modelo, ano) VALUES ('{carro.get('marca')}', '{carro.get('modelo')}', '{carro.get('ano')}')")
-    conexao.commit()
+    inserir(carro)
 
     return make_response(
         jsonify(
