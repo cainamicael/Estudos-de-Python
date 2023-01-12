19d11c4be843f774f1d04ae9dbbaff2b9822d2d0
cainamicael <cainamicaeloficial@gmail.com>
2023-01-12 15:54:52 -0300

    add

--- Aulas/Api com Banco de Dados (PycodeBR)/Api_de_Carros.py
+++ Aulas/Api com Banco de Dados (PycodeBR)/Api_de_Carros.py
@@ -1,28 +1,50 @@
+import sqlite3
 from flask import Flask, make_response , jsonify, request
-#from bd_fake import carros #Para usar o banco de dados fake 
+from bd_fake import carros #Para usar o banco de dados fake 
+
+conexao = sqlite3.connect('C:\\Users\\cmcai\\Documents\\GitHub\\Estudos de Python\\Estudos-de-Python\\Aulas\\Banco\\bd_carros.db', check_same_thread=False) #Esse segundo parâmetro é para não dar o erro de thread
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False #Para não deixar em ordem alfabética
 
 #Consultar carros
-@app.route('/carros',methods=['GET'])
+@app.route('/carros', methods=['GET'])
 def get_carros():
-    return make_response( #Para construir uma resposta
+    c = conexao.cursor()
+    c.execute("SELECT * FROM carros")
+    meus_carros = c.fetchall()
+
+    carros = list()
+    for carro in meus_carros:
+        carros.append({
+            'id' : carro[0],
+            'marca' : carro[1],
+            'modelo' : carro[2],
+            'ano' : carro[3]
+        })
+
+        return make_response(
         jsonify(
             mensagem = 'Lista de Carros',
             dado = carros
+            )
         )
-    )
 
-@app.route('/carros',methods=['POST'])
+#Criar carro
+@app.route('/carros', methods=['POST'])
 def create_carro():
-    novo_carro = request.get_json()
-    carros.append(novo_carro)
+    carro = request.json
+
+    c = conexao.cursor()
+    c.execute(f"INSERT INTO carros (marca, modelo, ano) VALUES ('{carro.get('marca')}', '{carro.get('modelo')}', '{carro.get('ano')}')")
+    conexao.commit()
+
     return make_response(
         jsonify(
-            mensagem = 'Carro cadastrado com sucesso!',
+            mensagem = 'Lista de Carros',
             dado = carros
-        )
+            )
         )
 
-app.run()
+app.run(port=5000, host='localhost',debug=True)
+conexao.close()
