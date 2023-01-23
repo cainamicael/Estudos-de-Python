df8400a8705e84ad4bfdb2755c918703a041c780
cainamicael <cainamicaeloficial@gmail.com>
2023-01-23 17:01:33 -0300

    add

--- Aulas/Aula66/Aula66.py
+++ Aulas/Aula66/Aula66.py
@@ -13,15 +13,24 @@ vnum_texto = StringVar()
 
 fr_quadro1 = Frame(app, borderwidth=1, relief='solid')
 """
-relief (flat, reised, sunken, solid)
+relief (flat, raised, sunken, solid)
 """
 fr_quadro1.place(x=10, y=10, width=300, height=100)
 
-Label(app, text='Tipo de cx(1, 2 ou 3').pack()
-tb_num = Entry(app, textvariable= vnum_texto)
-tb_num.pack()
+fr_quadro2 = Frame(app, borderwidth=1, relief='solid', background='#008')
+"""
+relief (flat, raised, sunken, solid)
+"""
+fr_quadro2.place(x=10, y=120, width=300, height=100)
 
-btn_msg =Button(app, text='Mostrar Mensagem', command= lambda:mostrarMsg(vnum_texto.get(), vmsg))#Para chamar função com argumentos chama como lambda
-btn_msg.pack()
+lb_tipo = Label(fr_quadro1, text='Tipo de cx(1, 2 ou 3')
+lb_tipo.place(x=10, y = 10)
+tb_num = Entry(fr_quadro1, textvariable= vnum_texto)
+tb_num.place(x = 10, y = 30)
+
+btn_msg =Button(fr_quadro2, text='Mostrar Mensagem', command= mostrarMsg)#Para chamar função com argumentos chama como lambda
+btn_msg.place(x=10, y = 50)
+
+
 
 app.mainloop()
